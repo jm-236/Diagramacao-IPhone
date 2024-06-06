3f0253e8e15683aa042d7d1f8d31e6e028594977
João Marcelo <159437965+jm-236@users.noreply.github.com>
2024-06-05 22:15:16 -0300

    feat: adição da interface reprodutor musical

--- Iphone/src/com/reprodutormusical/ReprodutorMusical.java
+++ Iphone/src/com/reprodutormusical/ReprodutorMusical.java
@@ -1,4 +1,11 @@
 package com.reprodutormusical;
 
 public interface ReprodutorMusical {
+
+    String musicaAtual = "";
+    boolean estaTocando = false;
+
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica(String musica);
 }
